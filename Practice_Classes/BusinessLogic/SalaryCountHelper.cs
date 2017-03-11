24aa2dc9b29abbbe6592d3459c7024dfd6b25686
Alexandr Sorokin <qlineb@gmail.com>
2017-03-11 15:45:18 +0700

    Created helper method SetSalary

--- Practice_Classes/BusinessLogic/SalaryCountHelper.cs
+++ Practice_Classes/BusinessLogic/SalaryCountHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,9 @@ namespace BusinessLogic
 {
     public static class SalaryCountHelper
     {
+        public static void SetSalary(Employee empl, double salary)
+        {
+            empl.Salary = salary * .87;
+        }
     }
 }
