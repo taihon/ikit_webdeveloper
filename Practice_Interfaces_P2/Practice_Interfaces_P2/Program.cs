90b08e2fcd341bda8a68911ee3729a8a289228e7
Alexandr Sorokin <qlineb@gmail.com>
2017-03-25 14:24:16 +0700

    empty solution to work on practice 25/03/2017

---
+++ Practice_Interfaces_P2/Practice_Interfaces_P2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Interfaces_P2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
