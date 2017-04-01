b9000eb46ff46208bbe494214a01a3ff9475ad30
Alexandr Sorokin <qlineb@gmail.com>
2017-04-01 14:32:18 +0700

    added Factory class

---
+++ Practice_Generics_Events/Domain/AnimalFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class AnimalFactory<T> 
+        where T : Animal, new()
+    {
+        public T Create()
+        {
+            return new T();
+        }
+    }
+}
