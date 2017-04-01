2d14c927d649415e58df4b987d794dbfd3801c89
Alexandr Sorokin <qlineb@gmail.com>
2017-04-01 15:45:03 +0700

    created manager class that will feed animals in zoo

---
+++ Practice_Generics_Events/Domain/Manager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Manager
+    {
+        public void OnHungry(Animal animal, HungryEventArgs args)
+        {
+            var foodType = args.IsMeatEater ? "Meat" : "Silo";
+            Console.WriteLine($"Fine. We'll bring {args.FoodAmount} of {foodType}");
+        }
+    }
+}
