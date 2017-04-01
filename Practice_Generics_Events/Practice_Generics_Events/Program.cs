314c78659c569d498b47cf2abd0e0b15e21339d0
Alexandr Sorokin <qlineb@gmail.com>
2017-04-01 16:46:25 +0700

    used zoo to illustrate event interaction between Horse and Zoo's personnel

--- Practice_Generics_Events/Practice_Generics_Events/Program.cs
+++ Practice_Generics_Events/Practice_Generics_Events/Program.cs
@@ -11,8 +11,13 @@ namespace Practice_Generics_Events
     {
         static void Main(string[] args)
         {
+            var vet = new Vet();
+            var mgr = new Manager();
+            var zoo = new Zoo { mgr = mgr, vet = vet };
             AnimalFactory<Horse> factory = new AnimalFactory<Horse>();
-            var horse = factory.Create();
+            var horse = factory.Create("Milly", zoo);
+            horse.Hungry();
+            vet.BeginVaccination();
         }
     }
 }
