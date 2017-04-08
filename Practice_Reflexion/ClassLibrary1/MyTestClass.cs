96d897ce9def04175d11bf719f46154191f81a51
Alexandr Sorokin <qlineb@gmail.com>
2017-04-08 14:51:18 +0700

    created base project structure for practice 08/04/2017

---
+++ Practice_Reflexion/ClassLibrary1/MyTestClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class MyTestClass
+    {
+        double _d, _f;
+
+        public MyTestClass(double d, double f)
+        {
+            _d = d;
+            _f = f;
+        }
+        public double Sum()
+        {
+            return _d + _f;
+        }
+        public void Info()
+        {
+            Console.WriteLine($"{_d}");
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
